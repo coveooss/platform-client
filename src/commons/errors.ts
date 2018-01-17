60b85437318fa028310e3dc7b60a3f586b0a761c
y-lakhdar <yassine.lakhdar.1@ulaval.ca>
2018-01-16 22:58:08 -0500

    ! FIxed typo

--- src/commons/errors.ts
+++ src/commons/errors.ts
@@ -7,7 +7,7 @@ export class StaticErrorMessage {
 
   // Rest
   public static FAILED_API_REQUEST: string = 'Unable to perform API request';
-  public static UNEXPECTED_RESPONSE: string = 'Unexepected response from the server';
+  public static UNEXPECTED_RESPONSE: string = 'Unexpected response from the server';
 
   // Fields
   public static MISSING_FIELD_NAME: string = 'Missing field name';
