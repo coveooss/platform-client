64ac9299bd52fc1941156bcff7324405378529cc
Yassine Lakhdar <ylakhdar@coveo.com>
2018-03-02 19:45:59 -0500

    Add lint plugin to webpack
    - Also some cleaning in the dev.js file

--- gulpTasks/dev.js
+++ gulpTasks/dev.js
@@ -2,10 +2,6 @@
 const nodemon = require('gulp-nodemon');
 const gulp = require('gulp');
 
-// var webpack = require('webpack');
-// var WebpackDevServer = require('webpack-dev-server');
-// var webpackConfig = require('../webpack.config.js');
-
 gulp.task('setupDevEnv', done => {
   gulp.src('environments/**/*.js').pipe(gulp.dest('./bin/config/env'));
   done();
@@ -23,25 +19,3 @@ gulp.task('devTest', ['watchTest'], () => {
   });
   return stream;
 });
-// gulp.task('dev', ['setupDevEnv', 'watchTS'], (done) => {
-
-//   webpackConfig.entry.unshift('webpack-dev-server/client?http://localhost:8080/');
-
-//   // Add Dev plugins
-//   // webpackConfig.plugins.push(plugins);
-
-//   const compiler = webpack(webpackConfig);
-
-//   new WebpackDevServer(compiler, {
-//     compress: true,
-//     contentBase: 'bin/',
-//     publicPath: 'http://localhost:8080/js/',
-//     disableHostCheck: true,
-//     stats: {
-//       colors: true,
-//       publicPath: true
-//     }
-
-//   }).listen(8080, 'localhost', () => {});
-//   done();
-// });
