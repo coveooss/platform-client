f9a910f429ff65dc5663c371919fd222664121c6
y-lakhdar <ylakhdar@coveo.com>
2019-05-15 10:11:23 -0400

    chore: Remove os condition from gup task

--- gulpTasks/compile.js
+++ gulpTasks/compile.js
@@ -3,9 +3,4 @@
 const gulp = require('gulp');
 const shell = require('gulp-shell');
 
-if (/^win/.test(process.platform)) {
-  gulp.task('compile', shell.task([`"node_modules/.bin/webpack" --process --progress --colors`]));
-} else {
-  gulp.task('compile', shell.task([`webpack --process --progress --colors`]));
-}
-
+gulp.task('compile', shell.task([`"node_modules/.bin/webpack" --process --progress --colors`]));
