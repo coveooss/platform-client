865fcadb19b40bdd4976dd5744e4a71352330111
y-lakhdar <ylakhdar@coveo.com>
2019-06-19 14:17:46 -0400

    chore: Remove unecessary dev options

--- gulpTasks/dev.js
+++ gulpTasks/dev.js
@@ -5,7 +5,7 @@ const shell = require('gulp-shell');
 const command = /^win/.test(process.platform)
   ? ['"node_modules/.bin/nyc" "node_modules/.bin/mocha-webpack" --webpack-config webpack.config-test.js --watch']
   : [
-      'cross-env NODE_ENV=test nyc mocha-webpack test/test.ts --watch src/**/*.ts --webpack-config webpack.config-test.js --recursive --require ts-node/register --mode development'
+      'cross-env NODE_ENV=test nyc mocha-webpack test/test.ts --watch src/**/*.ts --webpack-config webpack.config-test.js --recursive'
     ];
 
 gulp.task('dev', shell.task(command));
