b08d500098311dcf98396c9dfbc092034099d6c6
Yassine Lakhdar <ylakhdar@coveo.com>
2019-11-21 15:08:52 -0500

    ci: cI rework by including semantic-release
    Deletion of Gulp and shell release scripts. Future releases are now handled by semantic-release

--- gulpTasks/setup.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const gulp = require('gulp');
-
-const copyEnv = () => gulp.src('environments/**/*.js').pipe(gulp.dest('./bin/environments'));
-const copyViews = () => gulp.src('views/**/*.ejs').pipe(gulp.dest('./bin/views'));
-const copyGlobal = () => gulp.src('client-global.js').pipe(gulp.dest('./bin'));
-
-gulp.task('setup', gulp.series(copyEnv, copyGlobal, copyViews));
--- gulpTasks/test.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const gulp = require('gulp');
-const shell = require('gulp-shell');
-
-gulp.task(
-  'test',
-  shell.task(['cross-env NODE_ENV=test nyc mocha-webpack test/test.ts --webpack-config webpack.config-test.js --recursive --require ts-node/register'])
-);
