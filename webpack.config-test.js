c2e9e4776c29ffc56f35cc2395c4b1b648dd6ac4
Yassine Lakhdar <ylakhdar@coveo.com>
2020-10-19 12:01:57 -0400

    test: adjust unit tests for source commands

--- webpack.config-test.js
+++ webpack.config-test.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const WebpackNotifierPlugin = require('webpack-notifier');
 const buildCoverage = ['coverage', 'test'].indexOf(process.env.NODE_ENV) != -1;
+const nodeExternals = require('webpack-node-externals');
 
 const plugins = [];
 
@@ -9,20 +10,21 @@ plugins.push(new WebpackNotifierPlugin());
 module.exports = {
   name: 'client',
   target: 'node',
+  externals: [nodeExternals()],
   entry: './src/client.ts',
   mode: 'development',
   output: {
     path: path.resolve(__dirname, 'bin'),
     filename: 'test.js',
     library: 'PlatformClientTest',
-    publicPath: '/bin/test'
+    publicPath: '/bin/test',
   },
   resolve: {
     extensions: ['.ts', '.js'],
     alias: {
       env: __dirname + '/environments',
-      'this.env': 'test'
-    }
+      'this.env': 'test',
+    },
   },
   module: {
     noParse: /update-notifier/,
@@ -31,15 +33,15 @@ module.exports = {
         ? {
             test: /\.ts/,
             include: path.resolve('src'), // instrument only testing sources with Istanbul, after ts-loader runs
-            loader: 'istanbul-instrumenter-loader'
+            loader: 'istanbul-instrumenter-loader',
           }
         : [],
       { test: /\.ts$/, loader: 'ts-loader', exclude: /node_modules/ }
-    )
+    ),
   },
   plugins: plugins,
   bail: true,
   optimization: {
-    nodeEnv: false
-  }
+    nodeEnv: false,
+  },
 };
